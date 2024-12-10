fb26f3b1e713ecbbbd36ef0aa8b43a4e533e54b4
adel <adeelguitoun@gmail.com>
2024-12-10 18:35:52 +0100

    Setup website global styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,116 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+        gray: 'var(--gray)',
+        solitude: 'var(--solitude)',
+        tundora: 'var(--tundora)',
+        codgray: 'var(--cod--gray)',
+        dovegray30: 'var(--dove--gray-30)',
+        oceangreen: 'var(--ocean--green)',
+        celtic: 'var(--celtic)',
+        greenpea: 'var(--green--pea)',
+        jewel: 'var(--jewel)',
+        dodgerblue: 'var(--dodger--blue)',
+        azureradiance: 'var(--azure--radiance)',
+        scienceblue: 'var(--science--blue)',
+        endeavour: 'var(--endeavour)',
+        downriver: 'var(--downriver)',
+        amethyst: 'var(--amethyst)',
+        fuchsiapink: 'var(--fuchsia--pink)',
+        cerise: 'var(--cerise)',
+        disco: 'var(--disco)',
+        wineberry: 'var(--wine--berry)',
+        loulou: 'var(--loulou)',
+        revolver: 'var(--revolver)',
+        antiquebronze: 'var(--antique--bronze)',
+        bracken: 'var(--bracken)',
+        valencia: 'var(--valencia)',
+        brickred: 'var(--brick--red)',
+        crownofthorns: 'var(--crown-of--thorns)',
+        roseofsharon: 'var(--rose-of--sharon)',
+        brown: 'var(--brown)',
+        supernova: 'var(--supernova)',
+        tanhide: 'var(--tan--hide)',
+        silverchalice: 'var(--silver--chalice)',
+      },
+      fontFamily: {
+        Inter: ['var(--font-inter)'],
+        Silkscreen: ['var(--font-silkscreen)'],
+      },
+      fontSize: {
+        'heading-1': 'var(--inter-heading-1)',
+        'heading-2': 'var(--inter-heading-2)',
+        'regular-paragraph': 'var(--inter-paragraph)',
+      },
+      backgroundImage: {
+        main: 'url("/bg-img.png")',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      animation: {
+        gradient: 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          to: {
+            backgroundPosition: 'var(--bg-size) 0',
+          },
+        },
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
