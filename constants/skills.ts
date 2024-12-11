db438bf476eca63fd92a5ccf5ad531255917d9c4
adel <adeelguitoun@gmail.com>
2024-12-11 10:50:00 +0100

    Created skills constants

---
+++ constants/skills.ts
@@ -0,0 +1,74 @@
+import { Code, Cpu } from 'lucide-react';
+
+export const skills = [
+  {
+    id: 'scalability',
+    gradientColor: 'var(--jungle--green)',
+    asset: {
+      background: 'var(--bush)',
+      icon: Code,
+    },
+    title: {
+      label: 'Scalability',
+      background: 'var(--black--bean)',
+    },
+    content:
+      'A style library for React and/or React Native with a large typed superset of the React Native style API, with no outside dependencies in about 24Kb.',
+  },
+  {
+    id: 'static',
+    gradientColor: 'var(--azure--radiance)',
+    asset: {
+      background: 'var(--blue--zodiac)',
+      icon: Cpu,
+    },
+    title: {
+      label: 'Static',
+      background: 'var(--downriver)',
+    },
+    content:
+      'A smart optimizer that makes your app fast with partial analysis, extracts CSS, flattens your tree, and removes code. Next, Webpack, Vite, Babel and Metro.',
+  },
+  {
+    id: 'static',
+    gradientColor: 'var(--azure--radiance)',
+    asset: {
+      background: 'var(--blue--zodiac)',
+      icon: Cpu,
+    },
+    title: {
+      label: 'Static',
+      background: 'var(--downriver)',
+    },
+    content:
+      'A smart optimizer that makes your app fast with partial analysis, extracts CSS, flattens your tree, and removes code. Next, Webpack, Vite, Babel and Metro.',
+  },
+];
+
+export const slugs = [
+  'typescript',
+  'javascript',
+  'react',
+  'html5',
+  'css3',
+  'nodedotjs',
+  'express',
+  'nextdotjs',
+  'prisma',
+  'amazonaws',
+  'postgresql',
+  'firebase',
+  'vercel',
+  'testinglibrary',
+  'jest',
+  'cypress',
+  'docker',
+  'git',
+  'github',
+  'gitlab',
+  'visualstudiocode',
+  'figma',
+  'rubyonrails',
+  'ruby',
+  'mongodb',
+];
