2b1cb257ec78549f86865f3e9cb9ebc1c6b5f9f3
adel <adeelguitoun@gmail.com>
2024-12-12 08:57:12 +0100

    Added project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import About from '@/components/about/About';
+import Hero from '@/components/hero/Hero';
+import ProjectsSection from '@/components/projects/ProjectsSection';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Hero />
+      <About />
+      <ProjectsSection />
+    </>
+  );
 }
