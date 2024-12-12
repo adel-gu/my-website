e6711cc4904aeb71b060b0a392b56824513afc81
adel <adeelguitoun@gmail.com>
2024-12-12 08:57:56 +0100

    Created projects constants data

---
+++ constants/projects.ts
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    id: '01',
+    screenshot: '/dev-overflow.png',
+    title: 'dev overflow',
+    subTitle: 'Stack overflow clone',
+    stack: ['next.js', 'typescript', 'clerck', 'tailwindcss'],
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, aspernatur assumenda minima amet fugit iusto quo quia quaerat commodi quasi ab officiis possimus inventore nulla molestias, nobis deserunt neque officia.',
+    date: 'February 2022.',
+    github: '/',
+    live: '/',
+    color: 'rgba(249,115,22)',
+    btnBg: 'rgba(154,52,18,0.5)',
+    btnBgHover: 'rgba(67,20,7)',
+    cardBg: 'rgba(154,52,18,0.05)',
+  },
+  {
+    id: '02',
+    screenshot: '/meta-market.png',
+    title: 'Meta Market',
+    subTitle: 'NFT marketplace UI website',
+    stack: ['next.js', 'typescript', 'clerck', 'tailwindcss'],
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, aspernatur assumenda minima amet fugit iusto quo quia quaerat commodi quasi ab officiis possimus inventore nulla molestias, nobis deserunt neque officia.',
+    date: 'February 2022.',
+    github: '/',
+    live: '/',
+    color: 'rgba(139,92,246)',
+    btnBg: 'rgba(91,33,182,0.5)',
+    btnBgHover: 'rgba(46,16,101)',
+    cardBg: 'rgba(91,33,182,0.05)',
+  },
+];
+
+// color: 'violet-500',
+//     btnBg: 'violet-800/50',
+//     btnBgHover: 'violet-950',
+//     cardBg: 'violet-800/5',
