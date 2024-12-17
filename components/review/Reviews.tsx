7a33c5c28bbb1f0b10336aa674897e2996f41412
adel <adeelguitoun@gmail.com>
2024-12-17 14:48:57 +0100

    Update reviews heading

--- components/review/Reviews.tsx
+++ components/review/Reviews.tsx
@@ -5,7 +5,7 @@ const Reviews = () => {
   return (
     <section className="relative py-24">
       <div className="z-10 flex flex-col px-6 md:items-center md:justify-center">
-        <h3 className="h3-bold">Heartfelt Words from Incredible People</h3>
+        <h3 className="h3-bold">Sincere Words from Incredible People</h3>
       </div>
 
       <ReviewDemo />
