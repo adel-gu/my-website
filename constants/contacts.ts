6d83b8c8cc5bac1078c88322575b218af902e129
adel <adeelguitoun@gmail.com>
2024-12-17 16:46:13 +0100

    Update website static data

--- constants/contacts.ts
+++ constants/contacts.ts
@@ -1,5 +1,56 @@
 export const contacts = [
-  { id: 'github' },
-  { id: 'tweeter' },
-  { id: 'linkedin' },
+  {
+    id: 'tweeter',
+    title: 'X - tweeter',
+    icon: '/tweeter.svg',
+    description: 'Stay updated with my latest posts and thoughts.',
+    path: 'https://x.com/GuitounAdel',
+  },
+  {
+    id: 'linkedin',
+    title: 'LinkedIn',
+    icon: '/linkedin.svg',
+    description: "Let's connect and explore opportunities together.",
+    path: 'https://www.linkedin.com/in/adelguitoun/',
+  },
+  {
+    id: 'email',
+    title: 'Email',
+    icon: '/email.svg',
+    description: 'Feel free to reach out to me directly via email.',
+    path: 'mailto:adeelguitoun@gmail.com',
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 'resume',
+    icon: '/resume.svg',
+    path: 'https://x.com/GuitounAdel',
+  },
+  {
+    id: 'github',
+    icon: '/github.svg',
+    path: 'https://x.com/GuitounAdel',
+  },
+  {
+    id: 'tweeter',
+    icon: '/tweeter.svg',
+    path: 'https://x.com/GuitounAdel',
+  },
+  {
+    id: 'linkedin',
+    icon: '/linkedin.svg',
+    path: 'https://www.linkedin.com/in/adelguitoun/',
+  },
+  {
+    id: 'wellfound',
+    icon: '/wellfound.svg',
+    path: 'https://wellfound.com/profile/edit/overview',
+  },
+  {
+    id: 'medium',
+    icon: '/medium.svg',
+    path: 'https://wellfound.com/profile/edit/overview',
+  },
 ];
