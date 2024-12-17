d2d0fe27405c305c5fb9eece1bdc8a5522689923
adel <adeelguitoun@gmail.com>
2024-12-17 10:25:28 +0100

    Created tech tag card

---
+++ components/techs/TechTag.tsx
@@ -0,0 +1,20 @@
+import { MagicCard } from '../ui/magic-card';
+
+const TechTag = ({
+  gradientColor,
+  title,
+}: {
+  gradientColor: string;
+  title: string;
+}) => {
+  return (
+    <MagicCard
+      className="cursor-pointer bg-background w-fit border-none flex items-center justify-center py-2 px-2.5 "
+      gradientColor={gradientColor}
+      gradientOpacity={0.25}
+    >
+      <p className="text-whiteice text-sm font-Silkscreen">{title}</p>
+    </MagicCard>
+  );
+};
+export default TechTag;
