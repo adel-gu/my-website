3ab88bd6f74a6390dbe504196d1a4fb88388ab17
adel <adeelguitoun@gmail.com>
2024-12-17 16:25:36 +0100

    Added techs sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import Contact from '@/components/contact/Contact';
 import Hero from '@/components/hero/Hero';
 import ProjectsSection from '@/components/projects/ProjectsSection';
 import Reviews from '@/components/review/Reviews';
+import Techs from '@/components/techs/Techs';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Techs />
       <ProjectsSection />
       <Articles />
       <Reviews />
