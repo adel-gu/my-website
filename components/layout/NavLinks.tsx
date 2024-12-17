9a0fefe7818e157e6a56bd6e2f384f4e80a60828
adel <adeelguitoun@gmail.com>
2024-12-17 21:13:28 +0100

    Update blog link

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -1,27 +1,13 @@
-'use client';
-import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 const NavLinks = () => {
-  const pathname = usePathname();
-
   return (
     <nav className="flex flex-col gap-8 mt-16 font-Silkscreen text-3xl md:flex-row md:mt-0 md:text-sm">
-      <Link
-        className={`link ${
-          pathname === '/' ? 'active' : ''
-        } py-4 md:py-0 md:hover:opacity-70`}
-        href="/"
-      >
+      <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
         Projects
       </Link>
 
-      <Link
-        className={`link ${
-          pathname === '/' ? 'active' : ''
-        } py-4 md:py-0 md:hover:opacity-70`}
-        href="/about"
-      >
+      <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
         Blog
       </Link>
     </nav>
