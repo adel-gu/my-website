f5461ae03d5865f2fe6e3607fbb86d9c7e4c5050
Adel Guitoun <adeelguitoun@gmail.com>
2024-12-21 11:43:13 +0100

    Update dev-overflow live path

--- constants/projects.ts
+++ constants/projects.ts
@@ -16,7 +16,7 @@ export const projects = [
     description: `Dev-Overflow is a modern Stack Overflow clone designed for developers to share knowledge and solve problems collaboratively. Built with Next.js, TypeScript, and Tailwind CSS, it offers a seamless user experience with features like creating and answering questions, interactive likes, and commenting. The platform includes a scoring system to reward engagement and a rich 'What You See Is What You Get' (WYSIWYG) editor for writing detailed questions and answers with code snippets. Authentication and authorization are powered by Clerk, ensuring secure and personalized user interactions.`,
     date: 'Dec 5, 2024',
     github: 'https://github.com/adel-gu/dev-overflow',
-    live: 'https://dev-overflow-one-chi.vercel.app/',
+    live: 'https://dev-overflow-adels-projects-d623dcad.vercel.app/',
     color: 'rgba(249,115,22)',
     btnBg: 'rgba(154,52,18,0.5)',
     btnBgHover: 'rgba(67,20,7)',
